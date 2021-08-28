c70f7c928f2869b33a84edcc75679342f707894c
Alan Zieliński <zielinski.alan@outlook.com>
2021-08-28 22:02:57 +0200

    Added copying to clipboard after generate.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using SqlGenerator.Services;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -12,11 +13,21 @@ namespace SqlGenerator
     {
         const string RESULT_PATH = "Result.sql";
 
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        [STAThread]
         static async Task Main()
         {
             InitialDBTemplate template;
 
-            Console.WriteLine("[INFO] Pass the path to template");
+            Console.WriteLine("Pass the path to template.");
             string path = Console.ReadLine();
 
             if (string.IsNullOrEmpty(path))
@@ -36,13 +47,22 @@ namespace SqlGenerator
                 File.Create($"./{RESULT_PATH}").Close();
             }
 
+            var templateSQL = ScriptCreator.GenerateDB(template);
+
             using (StreamWriter sw = new($"./{RESULT_PATH}"))
             {
-                await sw.WriteAsync(ScriptCreator.GenerateDB(template));
+                await sw.WriteAsync(templateSQL);
             }
-
-            Console.WriteLine($"[INFO] Result is in {Assembly.GetExecutingAssembly().Location}\\{RESULT_PATH}");
             
+            //Copying MySQL script to the clippoard
+            OpenClipboard(IntPtr.Zero);
+            var ptr = Marshal.StringToHGlobalUni(templateSQL);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+            Marshal.FreeHGlobal(ptr);
+
+            Console.WriteLine($"[INFO] MySQL copied to your clipboard.");
+            Console.WriteLine($"[INFO] Result is in {Assembly.GetExecutingAssembly().Location}\\{RESULT_PATH}");
             Console.ReadLine();
         }
     }
